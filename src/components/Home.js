d65e64034a86141889565f88c033e0b9f218ec51
Sean Monaghan <smonagha@conncoll.edu>
2021-06-11 15:53:11 -0400

    amended schema to work with this project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,20 @@
 import React from "react"
+import image from "../images/computer.png"
 
 export default function Home () {
-    return <h1>Home Page</h1>
+    return (
+        <main>
+            <img 
+            src = {image} 
+            alt = "Banner" 
+            className = "absolute object-cover w-full h-full" 
+            />
+            <section className = "relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8 text-center">
+                <h1 className = "text-6xl homepage text-red-800 fond-bold leading-none lg:leading-tight home-name">
+                    Sean Monaghan<br />
+                    Fullstack Developer
+                </h1> 
+            </section>
+        </main>
+    )
 }
