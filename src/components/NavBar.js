7e6d8490226b1f14eb374bd51d53bf4c7297ef9a
Sean Monaghan <smonagha@conncoll.edu>
2021-06-11 22:25:56 -0400

    fixed navbar so that it is more responsive on small screens

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,62 @@
 import React from "react";
-import  {NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { SocialIcon } from "react-social-icons";
 
-
-export default function NavBar () {
-    return (
-        <header className = "bg-red-800">
-            <div className = "container mx-auto flex justify-between">
-                <nav className = "flex">
-                    <NavLink 
-                        to = "/"
-                        exact
-                        className = "inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-2xl font-bold cursive tracking-widest"
-                        activeClassName = "text-white"
-                    >
-                        Sean Monaghan
-                    </NavLink>
-                    <NavLink 
+export default function Navbar({ fixed }) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+      <nav className="sticky top-0 z-50 relative flex flex-wrap items-center justify-between px-2 py-3 bg-red-500 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <NavLink
+                to = "/"
+                className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                activeClassName = "text-white"
+            >
+              Sean Monaghan
+            </NavLink>
+            <NavLink 
                         to = "/post"
-                        className = "inline-flex items-center py-3 px-3 my-4 rounded text-red-200 hover:text-green-800"
+                        className = "inline-flex items-center py-3 px-3 my-4 rounded text-red-200 hover:text-gray-600"
                         activeClassName = "text-red-100 bg-red-700"
                     > 
                         Blog Posts
                     </NavLink>
                     <NavLink 
                         to = "/project"
-                        className = "inline-flex items-center py-3 px-3 my-4 rounded text-red-200 hover:text-green-800"
+                        className = "inline-flex items-center py-3 px-3 my-4 rounded text-red-200 hover:text-gray-800"
                         activeClassName = "text-red-100 bg-red-700"
                     >
                         Projects
                     </NavLink>
                     <NavLink 
                         to = "/about"
-                        className = "inline-flex items-center py-3 px-3 my-4 rounded text-red-200 hover:text-green-800"
+                        className = "inline-flex items-center py-3 px-3 my-4 rounded text-red-200 hover:text-gray-800"
                         activeClassName = "text-red-100 bg-red-700"
                     >
                         Contact
                     </NavLink>
-                </nav>
-                <div className = "inline-flex py-3 px-3 my-6 flex-col sm:flex-row ">
-                    <SocialIcon url = "https://www.linkedin.com/in/sean-monaghan-8318666b/" className = "mr-4 pb-2" target="_blank" fgColor = "#fff" style = {{ height : 35, width: 35 }} />
-                    <SocialIcon url = "https://github.com/Seanmonaghan" className = "mr-4" target="_blank" fgColor = "#fff" style = {{ height : 35, width: 35 }} />
-                    <SocialIcon url = "https://twitter.com/SeanMon83059815" className = "mr-4" target="_blank" fgColor = "#fff" style = {{ height : 35, width: 35 }} />
-                </div>
-            </div>
-        </header>
-    )
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-end items-center inline-flex py-2 px-2 my-3" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >       <SocialIcon url = "https://www.linkedin.com/in/sean-monaghan-8318666b/" className = "mr-4 pb-2" target="_blank" fgColor = "#fff" style = {{ height : 35, width: 35 }} />
+                  <SocialIcon url = "https://github.com/Seanmonaghan" className = "mr-4" target="_blank" fgColor = "#fff" style = {{ height : 35, width: 35 }} />
+                  <SocialIcon url = "https://twitter.com/SeanMon83059815" className = "mr-4" target="_blank" fgColor = "#fff" style = {{ height : 35, width: 35 }} />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -18,13 +18,13 @@ export default function Project () {
     }, [])
 
     return (
-        <main className = "bg-green-100 min-h-screen p-12">
+        <main className = "bg-gray-300 min-h-screen p-12">
             <section className = "container mx-auto">
                 <h1 className = "text-5xl flex justify-center cursive">MyProjects</h1>
                 <h2 className = "text-lg text-gray-600 flex justify-center mb-12">Welcome to my Project Page</h2>
                 <section className = "grid grid-cols-1 gap-8 sm:grid-cols-1 md:grid-cols-2">
                     {projectData && projectData.map((project, index) => (
-                    <article className = "relative rounded-lg shadow-xl bg-white p-16">
+                    <article className = "transition duration-500 ease-in-out hover:bg-gray-200 transform relative rounded-lg shadow-xl bg-white p-16 hover:-translate-y-1 hover:scale-105">
                         <h3 className = "text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
                             <a href={project.link} alt = {project.title} target = "_blank" rel="noopener noreferrer">{project.title}</a>
                         </h3>
